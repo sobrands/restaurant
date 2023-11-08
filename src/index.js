f14d7b4da0b10c138f98a06f66c8fba6f994f872
brandon <lim.wx.brandon@gmail.com>
2023-11-08 23:25:55 +0800

    Restructure file directories

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-import './style.css';
+import './styles/style.css';
+import initialPage from './pages/initialPage';
+
+function clear() {
+  const content = document.getElementById('content');
+  content.textContent = '';
+}
+
+// Initial Page Load
+clear();
+initialPage();
