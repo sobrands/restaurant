812c09fd754bd8d575a7840b625e026829e65340
brandon <lim.wx.brandon@gmail.com>
2023-11-08 23:25:43 +0800

    Loaded page through javascript instead

---
+++ src/pages/initialPage.js
@@ -0,0 +1,54 @@
+const tabArray = ['About', 'Menu', 'Contact'];
+
+export default function initialPage() {
+  const content = document.getElementById('content');
+
+  // Create Header and Navigation Bar content
+  const navBar = document.createElement('div');
+  navBar.setAttribute('id', 'nav-bar');
+
+  const logo = document.createElement('div');
+  logo.classList.add('logo');
+  logo.textContent = 'Bambino';
+
+  const tabs = document.createElement('div');
+  tabs.classList.add('tabs');
+
+  const tabList = document.createElement('ul');
+  for (let i=0; i<tabArray.length; i++) {
+    const listElement = document.createElement('li');
+    listElement.classList.add('selection');
+    listElement.textContent = tabArray[i];
+    tabList.appendChild(listElement);
+  }
+
+  // Create Main Body content
+  const mainContent = document.createElement('div');
+  mainContent.setAttribute('id', 'main-content');
+
+  const information = document.createElement('div');
+  information.classList.add('information');
+  information.setAttribute('id', 'intro');
+
+  const greeting = document.createElement('h1');
+  greeting.classList.add('greeting');
+  greeting.textContent = 'Welcome to paradise';
+
+  const description = document.createElement('p');
+  description.classList.add('description');
+  description.textContent = 'Food cooked with passion and fervour';
+
+  // Append elements into content 
+  tabs.appendChild(tabList);
+
+  navBar.appendChild(logo);
+  navBar.appendChild(tabs);
+
+  information.appendChild(greeting);
+  information.appendChild(description);
+
+  mainContent.appendChild(information);
+
+  content.appendChild(navBar);
+  content.appendChild(mainContent);
+}
